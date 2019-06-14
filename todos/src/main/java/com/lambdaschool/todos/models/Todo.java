a8cccf6d9de891d4e31cd302f78132bd2102e3d1
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-06-14 11:57:14 -0400

    Complete models, repos, and create controllers.

--- todos/src/main/java/com/lambdaschool/todos/models/Todo.java
+++ todos/src/main/java/com/lambdaschool/todos/models/Todo.java
@@ -1,4 +1,63 @@
 package com.lambdaschool.todos.models;
 
-public class Todo {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "todos")
+public class Todo extends Auditable
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long todoid;
+
+    @Column(nullable = false)
+    private String todo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userid",
+            nullable = false)
+    @JsonIgnoreProperties({"todos", "hibernateLazyInitializer"})
+    private User user;
+
+    public Todo()
+    {
+    }
+
+    public Todo(String quote, User user)
+    {
+        this.todo = todo;
+        this.user = user;
+    }
+
+    public long getQuotesid()
+    {
+        return todoid;
+    }
+
+    public void setTodoid(long todoid)
+    {
+        this.todoid = todoid;
+    }
+
+    public String getQuote()
+    {
+        return todo;
+    }
+
+    public void setTodo(String quote)
+    {
+        this.todo = todo;
+    }
+
+    public User getUser()
+    {
+        return user;
+    }
+
+    public void setUser(User user)
+    {
+        this.user = user;
+    }
 }
